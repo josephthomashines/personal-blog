19fc64f2b628e5f3dc5a9fc9e92670640c0dda93
Joseph Hines <jthines3@gmail.com>
2018-12-04 18:04:18 -0500

    Added prismjs

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,23 @@ module.exports = {
         icon: 'src/images/logo.png',
       },
     },
-    'gatsby-transformer-remark',
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: 'language-',
+              inlineCodeMarker: null,
+              aliases: {},
+              showLineNumbers: false,
+              noInlineHighlight: false,
+            },
+          },
+        ],
+      },
+    },
     'gatsby-plugin-sass',
     'gatsby-plugin-typescript',
     {
